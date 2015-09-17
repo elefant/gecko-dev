e55a3e9ff443c019edbd606fc052f49363421697
hchang <hchang@mozilla.com>
2015-09-17 15:41:58 +0800

    Tiny fixup for PackagedAppVerifier.

--- netwerk/protocol/http/PackagedAppVerifier.h
+++ netwerk/protocol/http/PackagedAppVerifier.h
@@ -92,22 +92,11 @@ public:
                       const nsACString& aSignature,
                       nsICacheEntry* aPackageCacheEntry);
 
-  bool GetIsPackageSigned() const
-  {
-    return mIsPackageSigned;
-  }
-
-  nsCString GetPackageOrigin() const
-  {
-    return mPackageOrigin;
-  }
-
-  // A internal use function to let the verifier know there's a broken
+  // A internal used function to let the verifier know there's a broken
   // last part.
   void SetHasBrokenLastPart(nsresult aStatusCode);
 
-  // Used to explicitly clear the listener so that there won't be a
-  // circular reference.
+  // Used to explicitly clear the listener to avoid circula reference.
   void ClearListener() { mListener = nullptr; }
 
   static const char* kSignedPakOriginMetadataKey;
